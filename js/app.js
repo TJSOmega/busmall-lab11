e1cc9a185a07febea33bb921964ceb3c3914a064
Terrence Jamal Simms <Tjs@omegacc.com>
2020-11-04 22:45:38 -0800

    fixed issues with busmall project

--- js/app.js
+++ js/app.js
@@ -8,6 +8,7 @@ var imageTwo = document.getElementById('imageTwo');
 var imageThree = document.getElementById('imageThree');
 
 var mainSection = document.getElementById('mainSection')
+var list = document.getElementById('list')
 
 var click = 0
 var totalClicks = 25
@@ -22,7 +23,7 @@ function Product(name) {
 };
 
 //Functions (Algorithm for 3 random products, Event Listener
- //Event Handler, )
+//Event Handler, )
 
 function getRandomproduct() {
   return Math.floor(Math.random() * productsList.length)
@@ -52,55 +53,84 @@ new Product('wine-glass');
 
 console.log(productsList)
 
-function productRandomAssignment(){
-  var prod1 = getRandomproduct();
-  var prod2 = getRandomproduct();
-  var prod3 = getRandomproduct();
+function productRandomAssignment() {
+    while (chosenProducts.length < 3) {
+    var uniqueProduct = getRandomproduct();
+    while (chosenProducts.includes(uniqueProduct)) {
+      uniqueProduct = getRandomproduct();
+    }
+    chosenProducts.push(uniqueProduct);
+  }
+  console.log('ChosenProduct Array', chosenProducts)
+}
 
-  prod1 = removeDuplicates(prod1, prod2, prod3);
-  prod2 = removeDuplicates(prod2, prod3, prod1);
-  prod3 = removeDuplicates(prod3, prod1, prod2);
+
+function renderProducts(){
+
+  chosenProducts = []
+
+  productRandomAssignment()
   
-  chosenProducts.push(prod1, prod2, prod3);
+  var prod1 = chosenProducts[0]
+  var prod2 = chosenProducts[1]
+  var prod3 = chosenProducts[2]
 
   imageOne.src = productsList[prod1].src;
   imageOne.alt = productsList[prod1].name;
+  productsList[prod1].views++;
 
   imageTwo.src = productsList[prod2].src;
   imageTwo.alt = productsList[prod2].name;
-  
+  productsList[prod2].views++;
+
   imageThree.src = productsList[prod3].src;
   imageThree.alt = productsList[prod3].name;
+  productsList[prod3].views++;
+};
 
+function renderResults() {
+  for (var i = 0; i < productsList.length; i++){
+    var li = document.createElement('li')
+  li.textContent = `${productsList[i].name} had ${productsList[i].votes} votes, and was seen ${productsList[i].views} times`
+  list.appendChild(li)
   }
 
-  
-  
-  function removeDuplicates(a, b, c) {
-    while (a === b || a === c) {
-      getRandomproduct(a);
-    }
-    return a;
-  }
+};
 
-productRandomAssignment()
+
+
+
+
+renderProducts()
+
+// productRandomAssignment()
 
 // console.log(chosenProducts)
- function eventHandler(event){
-
-   var productClick = event.target.alt;
-   click++;
 
 
-  for (var i = 0; i < productsList.length; i++){
+function eventHandler(event) {
+
+  var productClick = event.target.alt;
+  click++;
+
+
+  for (var i = 0; i < productsList.length; i++) {
     if (productClick === productsList[i].name) {
       productsList[i].votes++;
-
-      productRandomAssignment()
+      
     }
+
+    
   }
- }
-  
+  renderProducts();
+
+  if (click === totalClicks) {
+    mainSection.removeEventListener('click', eventHandler);
+
+    renderResults();
+  }
+}
+
 
 mainSection.addEventListener('click', eventHandler);
 
